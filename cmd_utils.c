127166df3fe865c2d27bcc02c0207550ee45dd86
lnicolli <lnicolli@c2r8s4.42lausanne.ch>
2023-11-29 17:43:24 +0100

    remove printf

--- cmd_utils.c
+++ cmd_utils.c
@@ -11,10 +11,8 @@ char **get_cmd(char *raw, char **path)
 	while(*path)
 	{
 		tmp = ft_strjoin(*path,cmd[0]);
-		printf("testing %s\n",tmp);
 		if (!access(tmp, X_OK))
 		{
-			printf("ex find for %s\n",tmp);
 			free(cmd[0]);
 			cmd[0] = tmp;
 			return(cmd);
