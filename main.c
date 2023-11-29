d0bc7e0a050b8a232bbfe74390b5f0487714b76d
lnicolli <lnicolli@c2r8s4.42lausanne.ch>
2023-11-29 13:49:12 +0100

    working with 2 cmd

--- main.c
+++ main.c
@@ -11,43 +11,49 @@
 
 int main(int argc, char **argv)
 {
-	int pipefd[2];
+	int pipefd[10][2];
 	t_input input;
+	int i_cmd;
+	pid_t pid;
+
+	i_cmd = 0;
+	int i_first = 0;
+	int i_last = 1;
 
 	parser(argc, argv, &input);
 
 	dup2(open(input.infile, O_RDONLY), STDIN_FILENO);
-	if (pipe(pipefd) == -1) {
+	if (pipe(pipefd[i_cmd]) == -1) {
         perror("pipe");
         exit(EXIT_FAILURE);
     }
-	pid_t pid1 = fork();
-    if (pid1 == -1)
+	while(i_cmd < 2)
 	{
-        perror("fork");
-        exit(EXIT_FAILURE);
-    }
-	if (pid1 == 0)
-	{
-		close(pipefd[0]);
-		dup2(pipefd[1], STDOUT_FILENO);
-		close(pipefd[1]);
-		execve(input.cmd1[0], input.cmd1, NULL);
-	}
-
-	pid_t pid2 = fork();
-    if (pid2 == -1)
-	{
-        perror("fork");
-        exit(EXIT_FAILURE);
-    }
-	if (pid2 == 0)
-	{
-		dup2(open(input.outfile, O_CREAT|O_WRONLY|O_TRUNC, S_IRUSR|S_IWUSR), STDOUT_FILENO);
-		close(pipefd[1]);
-		dup2(pipefd[0], STDIN_FILENO);
-		close(pipefd[0]);
-		execve(input.cmd2[0], input.cmd2, NULL);
+		pid = fork();
+		if (pid == -1)
+		{
+			perror("fork");
+			exit(EXIT_FAILURE);
+		}
+		if (pid == 0)
+		{
+			if (i_cmd == i_first)
+			{
+				close(pipefd[i_cmd][0]);
+				dup2(pipefd[i_cmd][1], STDOUT_FILENO);
+				close(pipefd[i_cmd][1]);
+				execve(input.cmd1[0], input.cmd1, NULL);
+			}
+			if (i_cmd == i_last)
+				dup2(open(input.outfile, O_CREAT|O_WRONLY|O_TRUNC, S_IRUSR|S_IWUSR), STDOUT_FILENO);
+			else
+				dup2(pipefd[i_cmd][0], STDOUT_FILENO);
+			close(pipefd[i_cmd - 1][1]);
+			dup2(pipefd[i_cmd - 1][0], STDIN_FILENO);
+			close(pipefd[i_cmd - 1][0]);
+			execve(input.cmd2[0], input.cmd2, NULL);
+		}
+		i_cmd ++;
 	}
 	return (0);
 }
