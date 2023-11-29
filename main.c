307c39e9bdb28da07633a85f06a321a68ba66e0e
lnicolli <lnicolli@c2r8s4.42lausanne.ch>
2023-11-29 12:41:29 +0100

    POC

--- main.c
+++ main.c
@@ -1,38 +1,23 @@
 #include <stdio.h>
 #include <sys/_types/_pid_t.h>
+#include <sys/fcntl.h>
 #include <sys/unistd.h>
 #include <unistd.h>
 #include "pipex.h"
 #include <sys/wait.h>
 #include <stdlib.h>
+#include <fcntl.h>
 
-
-void read_n_write(int fd_src, int fd_dst)
-{
-		int filefd = open("output.txt", O_WRONLY | O_CREAT, 0666);
-        if (filefd < 0) {
-            perror("open");
-            exit(EXIT_FAILURE);
-        }
-
-        char buffer[1024];
-        ssize_t bytes_read;
-        while ((bytes_read = read(pipefd[0], buffer, sizeof(buffer))) > 0) {
-            write(filefd, buffer, bytes_read);
-        }
-
-        close(filefd);
-        close(pipefd[0]);
-}
-
-//function to split the input and load it correctly in the struct
-//		if we get a cmd like "ls -l -a" we need to store it in a char** like ["ls","-l","-a", NULL]
 int main(int argc, char **argv)
 {
 	//t_input args;
+	//./pipex ola "/bin/ls -al" "/usr/bin/grep lib" ola
 	int pipefd[2];
-	char *args[] = {"/bin/ls", "-al", NULL};
+	t_input input;
 
+	parser(argc, argv, &input);
+
+	dup2(open(input.infile, O_RDONLY), STDIN_FILENO);
 	if (pipe(pipefd) == -1) {
         perror("pipe");
         exit(EXIT_FAILURE);
@@ -48,22 +33,27 @@ int main(int argc, char **argv)
 		close(pipefd[0]);
 		dup2(pipefd[1], STDOUT_FILENO);
 		close(pipefd[1]);
-		execve(args[0], args, NULL);
+		execve(input.cmd1[0], input.cmd1, NULL);
 	}
 
 	pid_t pid2 = fork();
+	int pipefd2[2];
+	if (pipe(pipefd2) == -1) {
+        perror("pipe");
+        exit(EXIT_FAILURE);
+    }
     if (pid2 == -1)
 	{
         perror("fork");
         exit(EXIT_FAILURE);
     }
-	char *args2[] = {"/usr/bin/grep", "main", NULL};
 	if (pid2 == 0)
 	{
+		dup2(open(input.outfile, O_CREAT|O_WRONLY|O_TRUNC), STDOUT_FILENO);
 		close(pipefd[1]);
 		dup2(pipefd[0], STDIN_FILENO);
 		close(pipefd[0]);
-		execve(args2[0], args2, NULL);
+		execve(input.cmd2[0], input.cmd2, NULL);
 	}
 	return (0);
 }
